5e9d8f93f41c694918e9316006de12e50483ef31
Petya Buchkova <petya.buchkova@gmail.com>
2019-12-12 18:50:46 +0100

    Fixed eslint file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,23 @@
 module.exports = {
-  // extends: ['plugin:prettier/recommended'],
-
-  // plugins: ['prettier'],
-  // env: {
-  //   browser: true,
-  //   node: true,
-  //   es6: true
-  // },
-  // rules: {
-  //   'prettier/prettier': 'error',
-  //   'function-paren-newline': 0,
-  //   'function-call-argument-newline': 0,
-  //   'object-curly-newline': [
-  //     'error',
-  //     {
-  //       ObjectExpression: 'never',
-  //       ObjectPattern: 'never',
-  //       ImportDeclaration: 'never',
-  //       ExportDeclaration: 'never'
-  //     }
-  //   ],
-  //   'object-property-newline': 0
-}
+  extends: ['plugin:prettier/recommended'],
+  plugins: ['prettier'],
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'function-paren-newline': 0,
+    'function-call-argument-newline': 0,
+    'object-curly-newline': [
+      'error',
+      {
+        ObjectExpression: 'never',
+        ObjectPattern: 'never',
+        ImportDeclaration: 'never',
+        ExportDeclaration: 'never'
+      }
+    ],
+    'object-property-newline': 0
 };
