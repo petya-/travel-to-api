07a630ef5aa056df0b2dc9c02b52ed1843e5ebae
Petya Buchkova <petya.buchkova@gmail.com>
2020-01-06 13:20:01 +0200

    Added FE link to welcome mail when a user signs up

--- app/Listeners/User.js
+++ app/Listeners/User.js
@@ -3,11 +3,11 @@
 const User = (exports = module.exports = {});
 const Mail = use('Mail');
 
-User.sendWelcomeEmail = async user => {
-  await Mail.send('emails.welcome', user.toJSON(), message => {
+User.sendWelcomeEmail = async (user, token) => {
+  await Mail.send('emails.welcome', { user, token }, message => {
     message
-      .to(user.email)
       .from('<from-email>')
+      .to(user.email)
       .subject('Welcome to TravelTo app!');
   });
 };
