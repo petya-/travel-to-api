5e80283e8e79d653059a1055514416f58012186f
Petya Buchkova <petya.buchkova@trafficlab.dk>
2020-07-11 12:51:35 +0200

    Updated version

--- config/database.js
+++ config/database.js
@@ -81,13 +81,14 @@ module.exports = {
     },
     pool: {
       min: 2,
-      max: 6,
-      createTimeoutMillis: 3000,
-      acquireTimeoutMillis: 30000,
-      idleTimeoutMillis: 30000,
-      reapIntervalMillis: 1000,
-      createRetryIntervalMillis: 100,
-      propagateCreateError: false, // <- default is true, set to false
+      max: 10,
+    },
+    migrations: {
+      tableName: 'adonis_schema',
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
     },
   },
 };
