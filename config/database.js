1c7c28821e8e284e5f70af694601a44c4b766239
Petya Buchkova <petya.buchkova@trafficlab.dk>
2020-07-11 12:19:27 +0200

    Added db configuration

--- config/database.js
+++ config/database.js
@@ -34,9 +34,9 @@ module.exports = {
     connection: {
       filename: Helpers.databasePath(
         `${Env.get('DB_DATABASE', 'development')}.sqlite`
-      )
+      ),
     },
-    useNullAsDefault: true
+    useNullAsDefault: true,
   },
 
   /*
@@ -56,8 +56,8 @@ module.exports = {
       port: Env.get('DB_PORT', ''),
       user: Env.get('DB_USER', 'root'),
       password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
-    }
+      database: Env.get('DB_DATABASE', 'adonis'),
+    },
   },
 
   /*
@@ -77,7 +77,17 @@ module.exports = {
       port: Env.get('DB_PORT', ''),
       user: Env.get('DB_USER', 'root'),
       password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
-    }
-  }
+      database: Env.get('DB_DATABASE', 'adonis'),
+    },
+    pool: {
+      min: 2,
+      max: 6,
+      createTimeoutMillis: 3000,
+      acquireTimeoutMillis: 30000,
+      idleTimeoutMillis: 30000,
+      reapIntervalMillis: 1000,
+      createRetryIntervalMillis: 100,
+      propagateCreateError: false, // <- default is true, set to false
+    },
+  },
 };
